9dec18b2a0f37c04cd53b415a5cadbb7ee8f955a
Ievgen Kapinos <ievgen.kapinos@gmail.com>
2021-01-17 18:51:41 +0200

    Injected restTemplate

--- src/main/java/com/holidu/interview/assignment/AppController.java
+++ src/main/java/com/holidu/interview/assignment/AppController.java
@@ -3,36 +3,46 @@ package com.holidu.interview.assignment;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestOperations;
 
 @Controller
 public class AppController
 {
-  @Value("${tree-radius.api-endpoint}")
-  private String apiEndpoint;
+  private final SearchRequest defaultSearchRequest;
   
-  @Value("${tree-radius.default.x}")
-  private double defaultX;
+  private final String apiEndpoint;
   
-  @Value("${tree-radius.default.y}")
-  private double defaultY;
+  private final RestOperations restTemplate;
   
-  @Value("${tree-radius.default.radius}")
-  private double defaultRadius;
+  @Autowired
+  public AppController(@Value("${tree-radius.api-endpoint}") String apiEndpoint,
+                       @Value("${tree-radius.default.x}") double defaultX, 
+                       @Value("${tree-radius.default.y}") double defaultY, 
+                       @Value("${tree-radius.default.radius}") double defaultRadius,
+                       RestTemplateBuilder restTemplateBuilder)
+  {
+    this.apiEndpoint = apiEndpoint;
+    this.defaultSearchRequest = new SearchRequest(defaultX, 
+                                             defaultY, 
+                                             defaultRadius);
+    
+    this.restTemplate = restTemplateBuilder.build();
+  }
   
   @GetMapping("/")
   public String index( Model model ) 
   {
     model.addAttribute("apiEndpoint", apiEndpoint);
-    model.addAttribute("defaultSearchRequest", new SearchRequest(defaultX, 
-                                                                 defaultY, 
-                                                                 defaultRadius));
+    model.addAttribute("defaultSearchRequest", defaultSearchRequest);
     return "index";
   }
   
@@ -41,6 +51,8 @@ public class AppController
   public Map<String, Integer> search(@RequestBody SearchRequest searchRequest) 
   {
     Map<String, Integer> response = new HashMap<>();
+    
+    
     
     response.put("name", 7);
     response.put("name2", 8);
